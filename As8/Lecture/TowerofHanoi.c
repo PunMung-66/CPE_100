aca6dca59cb496643c1ff729212902fb694e330a
PUN <punnawat.mungkalarungsi@gmail.com>
2023-10-04 20:10:27 +0700

    Add  new towerhanoi

--- As8/Lecture/TowerofHanoi.c
+++ As8/Lecture/TowerofHanoi.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-void TowerOfHanoi(int n, char source, char destination, char auxiliary)
+int TowerOfHanoi(int n, char source, char destination, char auxiliary)
 {
     if (n == 1)
+    {
         printf("Move disk 1 from source %c to destination %c\n", source, destination );
+        return (0);
+    }
     TowerOfHanoi(n-1, source, auxiliary, destination);
     printf("Move disk %d from source %c to destination %c\n", n, source, destination );
-    TowerOfHanoi(n-1, auxiliary, destination, source);
+    return TowerOfHanoi(n-1, auxiliary, destination, source);
 }
 
 int main()
