ee6d9f11fc148e6cf1162d60e66d9e4710de5758
PUN <punnawat.mungkalarungsi@gmail.com>
2023-10-29 22:04:02 +0700

    Add decodestring

--- Prac_Mid2/Decodestring.c
+++ Prac_Mid2/Decodestring.c
@@ -1,15 +1,40 @@
-// #include <stdio.h>
+#include <stdio.h>
 
+void decode(char * str)
+{
+    int i = 0, sum = 0;
+    while (str[i])
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            if ((!str[i + 1] >= '0' && str[i + 1] <= '9'))
+                sum += (str[i] - '0');
+            else
+            {
+                sum = (sum * 10) + (str[i] - '0');
+            }
+        }
 
+        else
+        {
+            if (!(str[i - 1] >= '0' && str[i - 1] <= '9'))
+                printf("%c", str[i]);
+            else
+            {
+                for (int a = 0; a < sum; a++)
+                    printf("%c", str[i]);
+            }
+            sum = 0;
+        }
+        i++;
+    }
 
-// int main()
-// {
-//     char* str;
-//     scanf("%s", str);
-//     while (*str)
-//     {
-//         if (*str >)
-//         str++;
-//     }
-    
-// }
+}
+
+int main()
+{
+    char str[100];
+    scanf("%s", str);
+    decode(str);
+    return (0);
+}
